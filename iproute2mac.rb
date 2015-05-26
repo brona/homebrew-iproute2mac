f7e09e0e0a3519604bcaa1aaeb3c855af9228c73
Bronislav Robenek <brona@robenek.me>
2015-05-26 23:36:11 +0200

    Fixed v1.0.3 Hash

--- iproute2mac.rb
+++ iproute2mac.rb
@@ -3,7 +3,6 @@ require "formula"
 class Iproute2mac < Formula
   homepage "https://github.com/brona/iproute2mac"
   url "https://github.com/brona/iproute2mac/archive/v1.0.3.zip"
-  sha1 "052628cb3debab40175c9cc5fa39588e33ab006e"
   sha1 "ba80de6e1c525c450ea6a33b069f11f65cbe784b"
 
   depends_on :python
