0f17095b42202704334b7e19bb6dee79715ea4a9
Bronislav Robenek <brona@robenek.me>
2015-11-01 12:32:03 +0100

    Bumped v1.0.9

--- iproute2mac.rb
+++ iproute2mac.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Iproute2mac < Formula
   homepage "https://github.com/brona/iproute2mac"
-  url "https://github.com/brona/iproute2mac/archive/v1.0.8.zip"
-  sha1 "9a8044b3fb1e1c2f410212e7138a61d662e75c3b"
+  url "https://github.com/brona/iproute2mac/archive/v1.0.9.zip"
+  sha1 "e4bc75c6dee574af221fcb4873b6cb0f22c88511"
 
   depends_on :python
 
