a11667878bba75c6dc490a47de5628fec167eb0f
Bronislav Robenek <brona@robenek.me>
2016-11-28 09:59:35 +0100

    Cloned formula from homebrew-core to get rid of SHA1 depracted e-mails

--- iproute2mac.rb
+++ iproute2mac.rb
@@ -1,17 +1,20 @@
-require "formula"
-
 class Iproute2mac < Formula
+  desc "CLI wrapper for basic network utilities on macOS - ip command"
   homepage "https://github.com/brona/iproute2mac"
-  url "https://github.com/brona/iproute2mac/archive/v1.1.0.zip"
-  sha1 "9b6b7992179b8cfb0daa90cd3a712893f5c3c68c"
+  url "https://github.com/brona/iproute2mac/archive/v1.1.1.tar.gz"
+  sha256 "1fe47faf3302f6d8ea45c9d59269dfed835580acefdb0c62cb14f0c70dcc66b1"
 
-  depends_on :python
+  bottle :unneeded
+
+  depends_on :python if MacOS.version <= :snow_leopard
 
   def install
     bin.install "src/ip.py" => "ip"
   end
 
   test do
+    system "#{bin}/ip", "route"
     system "#{bin}/ip", "address"
+    system "#{bin}/ip", "neigh"
   end
 end
